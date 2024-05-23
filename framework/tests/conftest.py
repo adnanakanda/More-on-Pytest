fa526c8a87ae27eaf042ae9bf92a525a9d50c6df
a.akanda <a.akanda@a1qa.com>
2024-05-23 10:37:23 +0600

    updated base test, settings file, test_data, added conftest

---
+++ framework/tests/conftest.py
@@ -0,0 +1,9 @@
+from browser.py_quality_services import PyQualityServices
+from core.utilities.json_settings_file import JsonSettingsFile
+
+from framework.utils.browser_factory import BrowserFactory
+
+
+def pytest_sessionstart(session):
+    PyQualityServices.browser_factory = BrowserFactory()
+    PyQualityServices.get_browser()
