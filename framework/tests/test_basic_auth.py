31544406e3d3ed4fb81d1aecaf340c92d98d44f3
a.akanda <a.akanda@a1qa.com>
2024-05-23 18:41:48 +0600

    data table

--- framework/tests/test_basic_auth.py
+++ framework/tests/test_basic_auth.py
@@ -3,6 +3,7 @@ from framework.forms.main_form import MainForm
 from framework.tests.base_test import Test_base
 from framework.forms.basic_auth_form import BasicAuthForm
 from framework.utils.basic_auth_utils import basic_auth
+
 class TestBasicAuth(Test_base):
 
     __main_form: MainForm = MainForm()
@@ -10,18 +11,14 @@ class TestBasicAuth(Test_base):
 
     @pytest.mark.trio
     async def test_basic_auth_wrapper(self, prepare_browser_factory):
-        browser = prepare_browser_factory
-        await basic_auth(browser)
-        self.__main_form.get_navigation_link("Basic Auth").click()
-        assert self.__basic_auth_form.is_success_msg_displayed(), "Success message is not displayed!"
+        connection = await basic_auth(prepare_browser_factory)
+        driver = prepare_browser_factory.driver
 
-
-        """try:
+        try:
+            self.__main_form.get_navigation_link("Basic Auth").click()
             assert self.__basic_auth_form.is_success_msg_displayed(), "Success message is not displayed!"
-        except AssertionError as e:
-            # Log the error or take other appropriate actions
-            # For example:
-            print(f"AssertionError: {e}")
-            # You can re-raise the exception if you want the test to fail
-            raise"""
-
+        finally:
+            try:
+                await connection.session.execute(connection.devtools.network.disable())
+            except:
+                pass
--- framework/utils/basic_auth_utils.py
+++ framework/utils/basic_auth_utils.py
@@ -3,12 +3,10 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.devtools.v85.network import Headers
 
-
-
-
 @pytest.mark.trio
-async def basic_auth(browser):
-    async with browser.driver.bidi_connection() as connection:
+async def basic_auth(prepare_browser_factory):
+    driver = prepare_browser_factory.driver
+    async with driver.bidi_connection() as connection:
         await connection.session.execute(connection.devtools.network.enable())
 
         credentials = base64.b64encode("admin:admin".encode()).decode()
@@ -16,7 +14,4 @@ async def basic_auth(browser):
 
         await connection.session.execute(connection.devtools.network.set_extra_http_headers(Headers(auth)))
 
-
-
-
-
+        return connection
