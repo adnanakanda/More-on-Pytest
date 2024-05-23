cf1ea8b1bf02c0432737e4ed46d33563c6cd92ff
a.akanda <a.akanda@a1qa.com>
2024-05-23 12:38:16 +0600

    Initial commit

--- framework/forms/basic_auth_form.py
+++ framework/forms/basic_auth_form.py
@@ -1,6 +1,19 @@
 from forms.base_form import BaseForm
-
-
+from browser.py_quality_services import PyQualityServices
+from selenium.webdriver.common.by import By
+from framework.locator_constants import LocatorConstant, ElementNameConstant
+from framework.tuple_utils import fill_locator_value
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class BasicAuthForm(BaseForm):
 
-    __page_name = "Basic Auth"
+    __page_name: str  = "Basic Auth"
+    __success_msg_elemnt = PyQualityServices.element_factory.get_leble((By.XPATH,LocatorConstant.PRECISE_TEXT_LOCATOR.format("Congratulations! You must have the proper credentials"),"Successful Authentication"))
+
+    def __init__(self):
+        super(BasicAuthForm, self).__init__(
+            (By.XPATH, LocatorConstant.PRECISE_TEXT_LOCATOR.format(self.__page_name)), self.__page_name)
+
+
+    def is_success_msg_displayed(self):
+        return self.__success_msg_elemnt.state.is_displayed
---
+++ framework/tests/test_basic_auth.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from browser.py_quality_services import PyQualityServices
+
+from framework.tests.base_test import Test_base
+from framework.forms.main_form import MainForm
+from framework.tests.base_test import Test_base
+from framework.forms.basic_auth_form import BasicAuthForm
+class TestBasicAuth(Test_base):
+
+    __main_form: MainForm = MainForm()
+    __basic_auth_form: BasicAuthForm = BasicAuthForm()
+
+
+
+    """def basic_authorisation(self, prepare_browser_factory):
+        browser = prepare_browser_factory
+        """
+
+    def test_basic_auth(self, prepare_browser_factory):
+        browser = prepare_browser_factory
+        self.__main_form.get_navigation_link("Basic Auth").click()
+        assert self.__basic_auth_form.is_success_msg_displayed(), "Success message is not displayed!"
+
