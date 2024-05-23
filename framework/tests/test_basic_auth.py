cda672bc3823503548f07ceece6d8b903a9a6b00
a.akanda <a.akanda@a1qa.com>
2024-05-23 15:40:53 +0600

    Implemented basic authorisation test

--- framework/tests/test_basic_auth.py
+++ framework/tests/test_basic_auth.py
@@ -2,19 +2,18 @@ import pytest
 from framework.forms.main_form import MainForm
 from framework.tests.base_test import Test_base
 from framework.forms.basic_auth_form import BasicAuthForm
+from framework.utils.basic_auth_utils import basic_auth
 class TestBasicAuth(Test_base):
 
     __main_form: MainForm = MainForm()
     __basic_auth_form: BasicAuthForm = BasicAuthForm()
 
 
-
-    """def basic_authorisation(self, prepare_browser_factory):
-        browser = prepare_browser_factory
-        """
-
     def test_basic_auth(self, prepare_browser_factory):
-        browser = prepare_browser_factory
-        self.__main_form.get_navigation_link("Basic Auth").click()
-        assert self.__basic_auth_form.is_success_msg_displayed(), "Success message is not displayed!"
+        @pytest.mark.trio
+        async def test_basic_auth_wrapper(self, prepare_browser_factory):
+            browser = prepare_browser_factory
+            await basic_auth(browser)
+            self.__main_form.get_navigation_link("Basic Auth").click()
+            assert self.__basic_auth_form.is_success_msg_displayed(), "Success message is not displayed!"
 
---
+++ framework/utils/basic_auth_utils.py
@@ -0,0 +1,22 @@
+import base64
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.devtools.v85.network import Headers
+
+
+
+
+@pytest.mark.trio
+async def basic_auth(browser):
+    async with browser.driver.bidi_connection() as connection:
+        await connection.session.execute(connection.devtools.network.enable())
+
+        credentials = base64.b64encode("admin:admin".encode()).decode()
+        auth = {'authorization': 'Basic ' + credentials}
+
+        await connection.session.execute(connection.devtools.network.set_extra_http_headers(Headers(auth)))
+
+
+
+
+
