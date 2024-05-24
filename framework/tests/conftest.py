6d182716ea2f022d1ec7d129963f13dc4ba67b46
a.akanda <a.akanda@a1qa.com>
2024-05-24 13:36:16 +0600

    updated conftest

--- framework/tests/conftest.py
+++ framework/tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from browser.py_quality_services import PyQualityServices
 from core.utilities.json_settings_file import JsonSettingsFile
 
@@ -7,3 +8,16 @@ from framework.utils.browser_factory import BrowserFactory
 def pytest_sessionstart(session):
     PyQualityServices.browser_factory = BrowserFactory()
     PyQualityServices.get_browser()
+
+@pytest.fixture(scope="session")
+def browser(request):
+    settings = JsonSettingsFile("config.json")
+    test_data = JsonSettingsFile("test_data.json")
+
+    browser = PyQualityServices.get_browser()
+    browser.maximize()
+    browser.go_to(settings.get_value("url"))
+
+    yield browser
+
+    browser.quit()
