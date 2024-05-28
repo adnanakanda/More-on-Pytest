ca1512d621519d530702cf6ac67b2de7d458c973
a.akanda <a.akanda@a1qa.com>
2024-05-28 19:10:03 +0600

    Initial commit on the SpotifyUI test

--- framework/tests/conftest.py
+++ framework/tests/conftest.py
@@ -1,6 +1,6 @@
+import pytest
 from browser.py_quality_services import PyQualityServices
 from core.utilities.json_settings_file import JsonSettingsFile
-
 from framework.utils.browser_factory import BrowserFactory
 
 
@@ -8,3 +8,14 @@ def pytest_sessionstart(session):
     PyQualityServices.browser_factory = BrowserFactory()
     PyQualityServices.get_browser()
 
+@pytest.fixture(scope="session", autouse = True)
+def browser(request):
+    settings = JsonSettingsFile("config.json")
+
+    browser = PyQualityServices.get_browser()
+    browser.maximize()
+    browser.go_to(settings.get_value("url"))
+
+    yield browser
+
+    browser.quit()
