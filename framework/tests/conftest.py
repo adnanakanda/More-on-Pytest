dea400e6206d8bb7e47e6e3cd50707a958a563f3
a.akanda <a.akanda@a1qa.com>
2024-05-31 17:53:09 +0600

    Moved the url into config.json, removed base_test

--- framework/tests/conftest.py
+++ framework/tests/conftest.py
@@ -1,26 +1,6 @@
 import pytest
-from browser.py_quality_services import PyQualityServices
-from core.utilities.json_settings_file import JsonSettingsFile
 
 from framework.utils.spotify_client import SpotifyClient
-from framework.utils.browser_factory import BrowserFactory
-
-
-def pytest_sessionstart(session):
-    PyQualityServices.browser_factory = BrowserFactory()
-    PyQualityServices.get_browser()
-
-@pytest.fixture(scope="session", autouse = True)
-def browser(request):
-    settings = JsonSettingsFile("config.json")
-
-    browser = PyQualityServices.get_browser()
-    browser.maximize()
-    browser.go_to(settings.get_value("url"))
-
-    yield browser
-
-    browser.quit()
 
 @pytest.fixture(scope="session")
 def api_client():
