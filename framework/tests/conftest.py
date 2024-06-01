1087c332af5272a0fd8267cf9c2e27129e92a8f5
a.akanda <a.akanda@a1qa.com>
2024-06-01 12:25:16 +0600

    rolling from merge from the master

--- framework/tests/conftest.py
+++ framework/tests/conftest.py
@@ -3,27 +3,8 @@ import pytest
 from browser.py_quality_services import PyQualityServices
 from core.utilities.json_settings_file import JsonSettingsFile
 from framework.utils.browser_factory import BrowserFactory
-
-
-def pytest_sessionstart(session):
-    PyQualityServices.browser_factory = BrowserFactory()
-    PyQualityServices.get_browser()
-
-@pytest.fixture(scope="session", autouse = True)
-def browser(request):
-    settings = JsonSettingsFile("config.json")
-
-    browser = PyQualityServices.get_browser()
-    browser.maximize()
-    browser.go_to(settings.get_value("url"))
-
-    yield browser
-
-    browser.quit()
-
+from framework.utils.spotify_client import SpotifyClient
 
 @pytest.fixture(scope="session")
-def base_url():
-    settings = JsonSettingsFile("config.json")
-    return settings.get_value("url")
-
+def api_client():
+    return SpotifyClient()
